cc483d81e56ef9fb6d58e3ddcf6b61785005bdf7
Taka-tennis <18e2331@gmail.com>
2026-07-27 17:19:34 +0900

    Firebase登録画面と空き時間入力機能を追加

---
+++ Tennis Connect/CoachRegisterView.swift
@@ -0,0 +1,18 @@
+//
+//  CoachRegisterView.swift
+//  Tennis Connect
+//
+//  Created by 松崎徹郎 on 2026/07/27.
+//
+
+import SwiftUI
+
+struct CoachRegisterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoachRegisterView()
+}
